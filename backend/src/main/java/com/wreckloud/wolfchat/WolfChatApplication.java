429140a16aa304ee939aaf01c4564e194d1a475b
雲之残骸 <1677820334@qq.com>
2026-01-05 15:39:29 +0800

    chore: 实行新的规划方案, 清理代码准备重构

--- backend/src/main/java/com/wreckloud/wolfchat/WolfChatApplication.java
+++ backend/src/main/java/com/wreckloud/wolfchat/WolfChatApplication.java
@@ -1,19 +1,13 @@
 package com.wreckloud.wolfchat;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan({
-    "com.wreckloud.wolfchat.account.infra.mapper",
-    "com.wreckloud.wolfchat.group.infra.mapper",
-    "com.wreckloud.wolfchat.admin.infra.mapper"
-})
 public class WolfChatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WolfChatApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WolfChatApplication.class, args);
+    }
 }
+
